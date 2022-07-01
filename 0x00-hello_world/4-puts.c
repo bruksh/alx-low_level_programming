1ca7a5ee39100ac0397254ec5046d4f7d2fc3963
bruksh <bruk1924sh@gmail.com>
2022-07-01 12:24:47 -0700

    puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"*
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {	
