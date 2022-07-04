c536ff85c5db2caac8501498fe5096363dfb4817
bruksh <bruk1924sh@gmail.com>
2022-07-04 13:20:26 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
-putchar('');
+putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
