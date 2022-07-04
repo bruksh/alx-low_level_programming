e9c8506144fb797bff46946a87c11eb9ea77e35d
bruksh <bruk1924sh@gmail.com>
2022-07-04 01:00:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Returns: Always 0.
+ */
+int main(void)
+{
+char d = 'a';
+
+while (d <= 'z')
+{
+putchar(d);
+d++;
+}
+putchar('\n')
+
+return (0);
+}
